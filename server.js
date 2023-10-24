7f5a8ef2518dbdc7056b43d50f6945f65f535208
BryanField15 <4bryanfield@gmail.com>
2023-10-24 14:57:50 +1100

    Working API's

---
+++ server.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('DB_PASSWORD', process.env.DB_PASSWORD);
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(DB, mongooseOptions)
+  .then(() => {
+    console.log('DB connection success!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Giddyup is galloping on port ${port}`);
+});
